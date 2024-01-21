9aecca942ecb5ad2fce6973226edc45a3d3fa962
LucasNT <lucasntakahashi@gmail.com>
2024-01-21 09:40:55 -0300

    feat(externals): add implementaion of TorrentEn...
    ...tryReader

---
+++ externals/torrent_entry_reader/csvEntryReader.go
@@ -0,0 +1,32 @@
+package CsvTorrentEntryReader
+
+import (
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/LucasNT/transmission-automation/interfaces"
+)
+
+type CsvTorrentEntryReader struct {
+	csvReader *csv.Reader
+}
+
+func NewCsvTorrentEntryReader(csvFile *os.File) CsvTorrentEntryReader {
+	var ret CsvTorrentEntryReader = CsvTorrentEntryReader{}
+	ret.csvReader = csv.NewReader(csvFile)
+	return ret
+}
+
+func (t CsvTorrentEntryReader) ReadTorrentEntry() (string, string, error) {
+	reader, err := t.csvReader.Read()
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			return "", "", interfaces.ErrNoTorrentEntry
+		}
+		return "", "", fmt.Errorf("Failed to read entry from csv file %w", err)
+	}
+	return reader[0], reader[1], nil
+}
